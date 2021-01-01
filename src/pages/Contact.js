0705f0eb54b11448eec9d856ccb26cebd07e22c7
Caroline Manson <carolinemanson15@gmail.com>
2021-01-01 16:34:54 -0500

    built about and contact pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,61 +1,37 @@
-import React, { Component } from "react";
-import API from "../utils/API";
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-solid-svg-icons'
+import { faGithubsquare } from '@fortawesome/free-solid-svg-icons'
 import Container from "../components/Container";
-import SearchForm from "../components/SearchForm";
-import SearchResults from "../components/SearchResults";
-import Alert from "../components/Alert";
+import Hero from "../components/Hero";
+import Row from "../components/Row";
+import Col from "../components/Col";
 
-class Search extends Component {
-  state = {
-    search: "",
-    breeds: [],
-    results: [],
-    error: ""
-  };
-
-  // When the component mounts, get a list of all available base breeds and update this.state.breeds
-  componentDidMount() {
-    API.getBaseBreedsList()
-      .then(res => this.setState({ breeds: res.data.message }))
-      .catch(err => console.log(err));
-  }
-
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    API.getDogsOfBreed(this.state.search)
-      .then(res => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch(err => this.setState({ error: err.message }));
-  };
-  render() {
-    return (
-      <div>
-        <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Search By Breed!</h1>
-          <Alert
-            type="danger"
-            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
-          >
-            {this.state.error}
-          </Alert>
-          <SearchForm
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            breeds={this.state.breeds}
-          />
-          <SearchResults results={this.state.results} />
-        </Container>
-      </div>
-    );
-  }
+function Contact() {
+  return (
+    <div>
+      <Hero backgroundImage="https://www.dropbox.com/s/zmfrn92tpve454b/background.jpeg?dl=0">
+        <h1>Caroline Manson</h1>
+      </Hero>
+      <Container style={{ marginTop: 30 }}>
+        <Row>
+          <Col size="md-12">
+            <h1>Find me on social media!</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+          <p>
+          <FontAwesomeIcon icon={faLinkedin} /> caroline-manson19
+          </p>
+            <p>
+            <FontAwesomeIcon icon={faGithubsquare} />carolinem15
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Search;
+export default Contact;
