5a7d93bd29045adb2862aee75048ec003ee9da7f
Caroline Manson <carolinemanson15@gmail.com>
2021-01-02 16:38:41 -0500

    got the background image to render

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import "./style.css";
-import background from "../public/background.jpeg"
 
 function Header(props) {
   return (
-    <div className="hero text-center" style={{
-      backgroundImage: `url(${background})`,
-      }}>
+    <div className="hero text-center">
         <h1>Caroline Manson</h1>
         <h2>{props.children}</h2>
     </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,18 +9,21 @@ import Col from "../components/Col";
 function Contact() {
   return (
     <div>
-      <Header backgroundImage="https://www.dropbox.com/s/zmfrn92tpve454b/background.jpeg?dl=0">
+      <Header>
         <h2>Find me on social media!</h2>
       </Header>
       <Container style={{ marginTop: 30 }}>
         <div className="row">
           <Col size="md-12">
-          <p className="text-center">
-          <FontAwesomeIcon icon={faLinkedin} /> caroline-manson19
+          <p className="text-center" >
+          <FontAwesomeIcon icon={faLinkedin} /> <a href="https://www.linkedin.com/in/caroline-manson19">caroline-manson19</a>
           </p>
             <p className="text-center">
-            <FontAwesomeIcon icon={faGithub} /> carolinem15
+            <FontAwesomeIcon icon={faGithub} /> <a href="https://github.com/carolinem15">carolinem15</a>
             </p>
+            <p className="text-center">carolinemanson15@gmail.com</p>
+            <p className="text-center">615-495-7513</p>
+            <p className="text-center"><a href="https://www.dropbox.com/s/68sq35hkedkfrqb/Caroline%27s%20Resume%20%284%29.pdf?dl=0">View my resume</a></p>
           </Col>
         </div>
       </Container>
