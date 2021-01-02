e724dc4c2eb0af4ae1bbe40a8f8cad1795771123
Caroline Manson <carolinemanson15@gmail.com>
2021-01-02 15:24:05 -0500

    fixed import in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Route } from "react-router-dom";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
